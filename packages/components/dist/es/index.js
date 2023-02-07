8335b248d91238e1d04114a4be4449486ac9834d
zhangshuai <1016868503@qq.com>
2023-02-07 21:23:29 +0800

    fix: 发布新版本

--- packages/components/dist/es/index.js
+++ packages/components/dist/es/index.js
@@ -2,38 +2,58 @@ import t from "./sbButton/index.js";
 import s from "./sbLink/index.js";
 import p from "./sbModal/index.js";
 import i from "./sbProgress/index.js";
-import n from "./sbSpin/index.js";
-import f from "./sbResult/index.js";
-import b from "./sbIcon/index.js";
+import f from "./sbSpin/index.js";
+import b from "./sbResult/index.js";
+import n from "./sbIcon/index.js";
 import e from "./sbCol/index.js";
 import a from "./sbRow/index.js";
-import c from "./sbBread/index.js";
-import l from "./sbBreadItem/index.js";
+import u from "./sbBread/index.js";
+import c from "./sbBreadItem/index.js";
 import d from "./sbUpload/index.js";
-import D from "./sbDropDown/index.js";
-import u from "./sbDropDownMenu/index.js";
-import w from "./sbDropDownItem/index.js";
-const o = [t, s, b, e, a, c, l, p, i, n, f, d, D, u, w], j = {
+import l from "./sbDropDown/index.js";
+import D from "./sbDropDownMenu/index.js";
+import M from "./sbDropDownItem/index.js";
+import w from "./sbMenu/index.js";
+import B from "./sbMenuItem/index.js";
+import I from "./sbSubMenu/index.js";
+import k from "./sbLayout/index.js";
+import x from "./sbHeader/index.js";
+import y from "./sbFooter/index.js";
+import A from "./sbAside/index.js";
+import L from "./sbMain/index.js";
+import R from "./sbBackTop/index.js";
+import S from "./sbAvatar/index.js";
+const o = [t, s, n, e, a, u, c, p, i, f, b, d, l, D, M, w, B, I, k, x, y, A, L, R, S], $ = {
   install: (m) => {
     for (const r in o)
       m.component(o[r].__name, o[r]);
   }
 };
 export {
-  j as default,
-  c as sbBread,
-  l as sbBreadItem,
+  $ as default,
+  A as sbAside,
+  S as sbAvatar,
+  R as sbBackTop,
+  u as sbBread,
+  c as sbBreadItem,
   t as sbButton,
   e as sbCol,
-  D as sbDropDown,
-  w as sbDropDownItem,
-  u as sbDropDownMenu,
-  b as sbIcon,
+  l as sbDropDown,
+  M as sbDropDownItem,
+  D as sbDropDownMenu,
+  y as sbFooter,
+  x as sbHeader,
+  n as sbIcon,
+  k as sbLayout,
   s as sbLink,
+  L as sbMain,
+  w as sbMenu,
+  B as sbMenuItem,
   p as sbModal,
   i as sbProgress,
-  f as sbResult,
+  b as sbResult,
   a as sbRow,
-  n as sbSpin,
+  f as sbSpin,
+  I as sbSubMenu,
   d as sbUpload
 };
--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,82 +1,84 @@
-// import { default as sbButton } from './sbButton';
+import { default as sbButton } from './sbButton';
 
-// import { default as sbLink } from './sbLink';
+import { default as sbLink } from './sbLink';
 
-// import { default as sbModal } from './sbModal';
+import { default as sbModal } from './sbModal';
 
-// import { default as sbProgress } from './sbProgress';
+import { default as sbProgress } from './sbProgress';
 
-// import { default as sbSpin } from './sbSpin';
+import { default as sbSpin } from './sbSpin';
 
-// import { default as sbResult } from './sbResult';
+import { default as sbResult } from './sbResult';
 
-// import { default as sbIcon } from './sbIcon';
+import { default as sbIcon } from './sbIcon';
 
-// import { default as sbCol } from './sbCol';
+import { default as sbCol } from './sbCol';
 
-// import { default as sbRow } from './sbRow';
+import { default as sbRow } from './sbRow';
 
-// import { default as sbBread } from './sbBread';
+import { default as sbBread } from './sbBread';
 
-// import { default as sbBreadItem } from './sbBreadItem';
+import { default as sbBreadItem } from './sbBreadItem';
 
-// import { default as sbUpload } from './sbUpload';
+import { default as sbUpload } from './sbUpload';
 
-// import { default as sbDropDown } from './sbDropDown';
+import { default as sbDropDown } from './sbDropDown';
 
-// import { default as sbDropDownMenu } from './sbDropDownMenu';
+import { default as sbDropDownMenu } from './sbDropDownMenu';
 
-// import { default as sbDropDownItem } from './sbDropDownItem';
+import { default as sbDropDownItem } from './sbDropDownItem';
 
-// import { default as sbMenu } from './sbMenu';
+import { default as sbMenu } from './sbMenu';
 
-// import { default as sbMenuItem } from './sbMenuItem';
+import { default as sbMenuItem } from './sbMenuItem';
 
-// import { default as sbSubMenu } from './sbSubMenu';
+import { default as sbSubMenu } from './sbSubMenu';
 
-// import { default as sbLayout } from './sbLayout';
+import { default as sbLayout } from './sbLayout';
 
-// import { default as sbHeader } from './sbHeader';
+import { default as sbHeader } from './sbHeader';
 
-// import { default as sbFooter } from './sbFooter';
+import { default as sbFooter } from './sbFooter';
 
-// import { default as sbAside } from './sbAside';
+import { default as sbAside } from './sbAside';
 
-// import { default as sbMain } from './sbMain';
+import { default as sbMain } from './sbMain';
 
-// import { default as sbBackTop } from './sbBackTop';
+import { default as sbBackTop } from './sbBackTop';
 
-// const components = [sbButton, sbLink, sbIcon, sbCol, sbRow, sbBread, sbBreadItem, sbModal, sbProgress, sbSpin, sbResult, sbUpload, sbDropDown, sbDropDownMenu, sbDropDownItem, sbMenu, sbMenuItem, sbSubMenu, sbLayout, sbHeader, sbFooter, sbAside, sbMain, sbBackTop];
-// export default {
-//   install: (app: any) => {
-//     for (const comkey in components) {
-//       app.component(components[comkey].__name, components[comkey]);
-//     }
-//   }
-// };
+import { default as sbAvatar } from './sbAvatar';
 
-// export { sbButton, sbLink, sbIcon, sbCol, sbRow, sbModal, sbProgress, sbSpin, sbResult, sbBread, sbBreadItem, sbUpload, sbDropDown, sbDropDownMenu, sbDropDownItem, sbMenu, sbMenuItem, sbSubMenu, sbLayout, sbHeader, sbFooter, sbAside, sbMain, sbBackTop };
+const components = [sbButton, sbLink, sbIcon, sbCol, sbRow, sbBread, sbBreadItem, sbModal, sbProgress, sbSpin, sbResult, sbUpload, sbDropDown, sbDropDownMenu, sbDropDownItem, sbMenu, sbMenuItem, sbSubMenu, sbLayout, sbHeader, sbFooter, sbAside, sbMain, sbBackTop, sbAvatar];
+export default {
+  install: (app: any) => {
+    for (const comkey in components) {
+      app.component(components[comkey].__name, components[comkey]);
+    }
+  }
+};
+
+export { sbButton, sbLink, sbIcon, sbCol, sbRow, sbModal, sbProgress, sbSpin, sbResult, sbBread, sbBreadItem, sbUpload, sbDropDown, sbDropDownMenu, sbDropDownItem, sbMenu, sbMenuItem, sbSubMenu, sbLayout, sbHeader, sbFooter, sbAside, sbMain, sbBackTop, sbAvatar };
 
 // 批量注册
 
-import type { App } from 'vue';
-const modules = import.meta.glob('./**/index.ts', { eager: true });
-interface component {
-  __file: string;
-  __hmrId: string;
-  install: Function;
-  render: Function;
-}
-type a = Record<string, component>;
-const exModules: a = {};
+// import type { App } from 'vue';
+// const modules = import.meta.glob('./**/index.ts', { eager: true });
+// interface component {
+//   __file: string;
+//   __hmrId: string;
+//   install: Function;
+//   render: Function;
+// }
+// type a = Record<string, component>;
+// const exModules: a = {};
 
-export default {
-  install: (app: App) => {
-    Object.entries(modules).forEach((module: any[]) => {
-      const name = module[0].split('/')[1];
-      exModules[name] = module[1].default;
-      app.use(module[1].default);
-    });
-  },
-  ...exModules
-};
+// export default {
+//   install: (app: App) => {
+//     Object.entries(modules).forEach((module: any[]) => {
+//       const name = module[0].split('/')[1];
+//       exModules[name] = module[1].default;
+//       app.use(module[1].default);
+//     });
+//   },
+//   ...exModules
+// };
