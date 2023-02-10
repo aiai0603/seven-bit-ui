15d9eb6aa737b997b8e505b4aa84e2c488a0ef34
Lemonadeccc <zwjhb12@163.com>
2023-02-10 20:25:09 +0800

    fix: 解决提交冲突

--- examples/app.vue
+++ examples/app.vue
@@ -1,17 +1,4 @@
 <template>
- <div>
-  <sb-input size="small" disabled="true">
-    <template #prepend>
-            http://
-        </template>
-  </sb-input>
-<br>
-  <sb-input size="small">
-    <template #append>
-            .com
-    </template>
-  </sb-input>
- </div>
   <div>
     <Table />
     <Avatar />
