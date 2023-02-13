a0b2758c7cd079fd28a66b8a8ddc916dfb888dc9
Lemonadeccc <zwjhb12@163.com>
2023-02-13 19:05:45 +0800

    fix: 修复sbDrawer的zIndexbug。

--- examples/app.vue
+++ examples/app.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
-      <Table />
-      <Avatar />
-    </div>
-  </template>
-  <script lang="ts" setup>
-    import Table from './components/table.vue';
-    import Avatar from './components/avatar.vue';
-  </script>
+      <div>
+        <Table />
+        <Avatar />
+      </div>
+    </template>
+    <script lang="ts" setup>
+      import Table from './components/table.vue';
+      import Avatar from './components/avatar.vue';
+    </script>
